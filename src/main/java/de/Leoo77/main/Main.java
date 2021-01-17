08670436be01a0e1e8aa482e7119a5d0835894da
Leoo77 <35805197+Leoo77@users.noreply.github.com>
2021-01-17 22:57:27 +0100

    added no Cap feature

--- src/main/java/de/Leoo77/main/Main.java
+++ src/main/java/de/Leoo77/main/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FallbackLoggerConfiguration.setTrace(true);
-        FallbackLoggerConfiguration.setDebug(true);
+//        FallbackLoggerConfiguration.setTrace(true);
+//        FallbackLoggerConfiguration.setDebug(true);
 
         DiscordApi api = new DiscordApiBuilder().setToken(BotProperties.getInstance().getToken()).login().join();
         api.addListener(new MessagesListener());
